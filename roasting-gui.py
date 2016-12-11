13a7a8b7c4548f36efc08ca0a7fe3505c0dc1681
Rebecca Pledger <rebeccapledger14@gmail.com>
2016-12-11 15:20:46 -0600

    Testing adding a quit button

--- roasting-gui.py
+++ roasting-gui.py
@@ -1,16 +1,24 @@
-from Tkinter import Tk, Frame, BOTH
+from Tkinter import Tk, BOTH
+from ttk import Frame, Button, Style
 
 class Example(Frame):
 	def __init__(self, parent):
-		Frame.__init__(self, parent, background="white")
+		Frame.__init__(self, parent)
 
 		self.parent = parent
-
 		self.initUI()
 
 	def initUI(self):
-		self.parent.title("Simple")
+
+		sw = self.parent.title = ("Quit Button")
+		self.style = Style()
+		self.style.theme_use("default")
+		
 		self.pack(fill=BOTH, expand=1)
+
+		quitButton = Button(self, text="Quit",
+			command=self.quit)
+		quitButton.place(x=50, y=50)
 
 def main():
  	root = Tk()
